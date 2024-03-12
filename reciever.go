9302c9a645f77ba20117bdcdea5def92d276ce10
Yuke Liu <60868124+Yuke-Liu@users.noreply.github.com>
2024-03-12 16:47:58 -0700

    Update reciever.go to keep receiving if its pre dump
    Signed-off-by: Yuke Liu <60868124+Yuke-Liu@users.noreply.github.com>

--- reciever.go
+++ reciever.go
@@ -16,64 +16,66 @@ func receive() {
 		log.Fatal(err)
 	}
 	defer listener.Close()
-
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	// Read the dumpType value
-	var dumpType int32
-	err = binary.Read(conn, binary.BigEndian, &dumpType)
-	if err != nil {
-		log.Fatal("Failed to read dump type:", err)
-	}
-
-	// Receive and unzip the file
-	gzipReader, err := gzip.NewReader(conn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer gzipReader.Close()
-
-	tarReader := tar.NewReader(gzipReader)
-
-	for {
-		header, err := tarReader.Next()
-		if err == io.EOF {
-			break // End of archive
-		}
+	for{
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		switch header.Typeflag {
-		case tar.TypeDir:
-			if err := os.Mkdir(header.Name, 0755); err != nil {
-				log.Fatal(err)
+		defer conn.Close()
+	
+		// Read the dumpType value
+		var dumpType int32
+		err = binary.Read(conn, binary.BigEndian, &dumpType)
+		if err != nil {
+			log.Fatal("Failed to read dump type:", err)
+		}
+	
+		// Receive and unzip the file
+		gzipReader, err := gzip.NewReader(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer gzipReader.Close()
+	
+		tarReader := tar.NewReader(gzipReader)
+	
+		for {
+			header, err := tarReader.Next()
+			if err == io.EOF {
+				break // End of archive
 			}
-		case tar.TypeReg:
-			file, err := os.Create(header.Name)
 			if err != nil {
 				log.Fatal(err)
 			}
-			if _, err := io.Copy(file, tarReader); err != nil {
-				log.Fatal(err)
+	
+			switch header.Typeflag {
+			case tar.TypeDir:
+				if err := os.Mkdir(header.Name, 0755); err != nil {
+					log.Fatal(err)
+				}
+			case tar.TypeReg:
+				file, err := os.Create(header.Name)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if _, err := io.Copy(file, tarReader); err != nil {
+					log.Fatal(err)
+				}
+				file.Close()
 			}
-			file.Close()
 		}
-	}
-
-	log.Printf("File received successfully. Dump type: %d\n", dumpType)
-
-	// Decide what to do with the file based on the dumpType value
-	// For example:
-	switch dumpType {
-	case 1:
-		log.Println("Performing action for dump type 1")
-		// Add your logic here
-	case 2:
-		log.Println("Performing action for dump type 2")
+	
+		log.Printf("File received successfully. Dump type: %d\n", dumpType)
+	
+		// Decide what to do with the file based on the dumpType value
+		// For example:
+		switch dumpType {
+		case 1:
+			log.Println("Performing action for pre dump")
+			// Add your logic here
+		case 2:
+			log.Println("Performing action for dump")
+			return
+		}
 	}
 }
